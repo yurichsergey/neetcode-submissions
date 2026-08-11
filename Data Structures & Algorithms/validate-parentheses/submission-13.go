477dc9b4144927749416629f9cece787df7e5b69
Sergey <yurichsergey@gmail.com>
2026-08-11 20:52:36 +0400

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.go
@@ -0,0 +1,20 @@
+func isValid(s string) bool {
+    if (len(s) % 2 != 0) {
+        return false;
+    }
+    st := make([]rune, len(s))
+    for _, c := range(s) {
+        if c == '(' {
+            st = append(st, ')')
+        } else if c == '[' {
+            st = append(st, ']')
+        } else if c == '{' {
+            st = append(st, '}')
+        } else if len(st) == 0 || c != st[len(st) - 1] {
+            return false
+        } else {
+            st = st[:len(st) - 1]
+        }
+    }
+    return true;
+}
