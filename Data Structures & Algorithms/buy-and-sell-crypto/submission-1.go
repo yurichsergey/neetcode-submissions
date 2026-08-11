fd60cf8d931fb0bfad5551039e069aa608c8d92b
Sergey <yurichsergey@gmail.com>
2026-08-11 20:51:19 +0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.go
@@ -0,0 +1,14 @@
+func maxProfit(prices []int) int {
+    bestProfit := 0
+    lowestPrice := prices[0]
+    for i := 1; i < len(prices)-1; i++ {
+        currentPrice := prices[i]
+        currentProfit := currentPrice - lowestPrice
+        if currentPrice < lowestPrice {
+            lowestPrice = currentPrice
+        } else if currentProfit > bestProfit {
+            bestProfit = currentProfit
+        }
+    }
+    return bestProfit
+}
